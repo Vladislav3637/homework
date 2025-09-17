60427b7a7277f7f6faf9bf98941e038de2b387e2
Vladislav3637 <1998vladd@gmail.com>
2025-09-17 21:58:23 +0300

    Update main.js

--- homework14/main.js
+++ homework14/main.js
@@ -1,7 +1,15 @@
 /* 1-е задание */
 console.log('1-е задание:');
-const firstName = 'Влад', age = 27, profession = 'программист',cityOfResidence = 'Москва',favoriteHobby = "саморазвитие";
-console.log(`Имя: ${firstName}; возраст: ${age}; профессия: ${profession}; город: ${cityOfResidence}; хобби: ${favoriteHobby}`);
+const me = [
+    {
+        firstName: 'Влад',
+        age: 27,
+        profession: 'программист',
+        cityOfResidence: 'Москва',
+        favoriteHobby: "саморазвитие"
+    }
+]
+console.log(`Имя: ${me[0].firstName}; возраст: ${me[0].age}; профессия: ${me[0].profession}; город: ${me[0].cityOfResidence}; хобби: ${me[0].favoriteHobby}`);
 
     /* 2-е задание */
     console.log(' ')
@@ -48,4 +56,5 @@ for (let i = 0; i < users.length; i++) {
         numberNoAdmin++
     }
 }
+
 console.log(`Количество обычных пользователей: ${numberNoAdmin}`)
